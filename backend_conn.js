4e6e8977dc16a10477f4fef5223c1e99fa6889aa
Ritz97 <ritam.de1997@gmail.com>
2018-07-03 22:24:23 +0530

    Update backend_conn.js
    This file receives the parameter 'json_param', which consists of all the available extracted parameters from Dialogflow API after an initial conversation with the user. To check for available devices, the necessary parameters are passed to backend, along with a retrieved DeviceID. The number of available devices with the associated specifications are finally returned back.
    
    Function for issuing a device is yet to be developed.
    Problems: Where to store the retrieved parameters for the second function call while issuing the device.

--- backend_conn.js
+++ backend_conn.js
@@ -1,25 +1,22 @@
 //Importing the backend function
 const backindex = require(./index);
 
-const express = require('express');
-const bodyparser = require('body-parser');
-const app = express();
-app.use(bodyparser.json());
-
-bkconn.exports = function(json_param) {
+//Function to be called by Middleware main
+exports.bkconn = function(json_param) {
 
 	if (json_param.issue == true) {
-		var devissue = {};
-		
-		
+		//Issue device
 	}
 	
 	else {
 		//Query for available devices
-		var devqr = {"device_name" : json_param.device_name, "ram" : json_param.ram, "storage" : json_param.storage};
-		var dev_details = index.query_device(devqr);
-		var unitqr = {"device_id" : dev_details.device_id, "os" : json_param.os, "os_version" : json_param.os_version, "employeeregistrationid" : "none"};
-		var unit_details = index.query_unit(unitqr);
-		return noofavailable;
+		var devqr = {"RAM" : json_param.ram, "Storage" : json_param.storage};
+		var dev_details = backindex.query_device(devqr);
+
+		//Uses the device id obtained from above function call
+		var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.os, "EmployeeRegistrationID" : "none"};
+		var unit_details = backindex.query_unit(unitqr);
+		
+		return unit_details.NoOfAvailable;
 	}
 }
