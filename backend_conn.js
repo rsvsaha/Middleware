646ea8536f1e429013f059e318979621d86b5b22
Ritz97 <ritam.de1997@gmail.com>
2018-07-03 07:39:37 +0000

    Update backend_conn.js

--- backend_conn.js
+++ backend_conn.js
@@ -1,3 +1,6 @@
+//Importing the backend function
+const backindex = require(./index);
+
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
@@ -6,16 +9,17 @@ app.use(bodyparser.json());
 bkconn.exports = function(json_param) {
 
 	if (json_param.issue == true) {
-	
-		app.post('/unit/issue', (req, res) => {
-			//Query for issuing
-		});
+		var devissue = {};
+		
+		
 	}
 	
 	else {
-	
-		app.post('/query/device', (req, res) => {
-			//Query for availability
-		});
+		//Query for available devices
+		var devqr = {"device_name" : json_param.device_name, "ram" : json_param.ram, "storage" : json_param.storage};
+		var dev_details = index.query_device(devqr);
+		var unitqr = {"device_id" : dev_details.device_id, "os" : json_param.os, "os_version" : json_param.os_version, "employeeregistrationid" : "none"};
+		var unit_details = index.query_unit(unitqr);
+		return noofavailable;
 	}
 }
