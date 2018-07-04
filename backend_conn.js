66379c4ca036ecdd3c8e906bca23ee6833bd65a3
Ritz97 <ritam.de1997@gmail.com>
2018-07-04 21:37:29 +0530

    Update backend_conn.js

--- backend_conn.js
+++ backend_conn.js
@@ -2,34 +2,32 @@
 const backindex = require(./index);
 
 //Function to be called by Middleware main
-exports.bkconn = function(json_param, function(err, result) {
+exports.bkconn = function(json_param, callback) {
 	
-	if(!err) {
-		if (json_param.issue == true) {
-			//Issue device
+	if (json_param.issue == true) {
+		//Issue device
 		
-			var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
-			var dev_details = backindex.query_device(devqr);
+		var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
+		var dev_details = backindex.query_device(devqr);
 		
-			//Uses the device id obtained from above function call
-			var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.parameters.os, "EmployeeRegistrationID" : "none"};
-			var unit_details = backindex.issue_unit(unitqr);
+		//Uses the device id obtained from above function call
+		var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.parameters.os, "EmployeeRegistrationID" : "none"};
+		var unit_details = backindex.issue_unit(unitqr);
 			
-			result = unit_details.unitID;
-			callback(result);
-		}
-	
-		else {
-			//Query for available devices
-			var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
-			var dev_details = backindex.query_device(devqr);
-
-			//Uses the device id obtained from above function call
-			var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.parameters.os, "EmployeeRegistrationID" : "none"};
-			var unit_details = backindex.query_unit(unitqr);
-		
-			result = unit_details.NoOfAvailable;
-			callback(result);
-		}
+		result = unit_details.unitID;
+		callback(result);
 	}
-});
+	
+	else {
+		//Query for available devices
+		var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
+		var dev_details = backindex.query_device(devqr);
+
+		//Uses the device id obtained from above function call
+		var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.parameters.os, "EmployeeRegistrationID" : "none"};
+		var unit_details = backindex.query_unit(unitqr);
+		
+		result = unit_details.NoOfAvailable;
+		callback(result);
+	}	
+};
