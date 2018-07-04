6c53ee24ab4bc0e855964e73a8531bf2e48e1291
Ritz97 <ritam.de1997@gmail.com>
2018-07-04 05:15:36 +0000

    Update backend_conn.js

--- backend_conn.js
+++ backend_conn.js
@@ -6,15 +6,24 @@ exports.bkconn = function(json_param) {
 
 	if (json_param.issue == true) {
 		//Issue device
+		var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
+		var dev_details = backindex.query_device(devqr);
+		
+		//Uses the device id obtained from above function call
+		var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.parameters.os, "EmployeeRegistrationID" : "none"};
+		var unit_details = backindex.issue_unit(unitqr);
+		
+		return unit_details.unitID;
+		
 	}
 	
 	else {
 		//Query for available devices
-		var devqr = {"RAM" : json_param.ram, "Storage" : json_param.storage};
+		var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
 		var dev_details = backindex.query_device(devqr);
 
 		//Uses the device id obtained from above function call
-		var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.os, "EmployeeRegistrationID" : "none"};
+		var unitqr = {"DeviceID" : dev_details.DeviceID, "OS" : json_param.parameters.os, "EmployeeRegistrationID" : "none"};
 		var unit_details = backindex.query_unit(unitqr);
 		
 		return unit_details.NoOfAvailable;
