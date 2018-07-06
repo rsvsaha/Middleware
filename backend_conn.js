d00adb1c8f91acfc9a2646e374cb14d6db4e6b26
Ritz97 <ritam.de1997@gmail.com>
2018-07-06 23:20:55 +0530

    Update backend_conn.js

--- backend_conn.js
+++ backend_conn.js
@@ -1,45 +1,80 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
 //Importing the backend function
-//const backindex = require(./index);
+const router = require('./DEREK_router');
+
 
 //Function to be called by Middleware main
 exports.bkconn = function(json_param, callback) {
+
+	//Query for DeviceID
+	var devqr = {"Accessories" : json_param.parameters.Accessories, "DeviceType" : json_param.parameters.DeviceType, "OS" : json_param.parameters.OS, "OSVersion" : json_param.parameters.OSVersion, "Maker" : json_param.parameters.maker, "Model" : json_parameters.Model, "RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage};
 	
-	console.log(json_param);
+	request({
+		url:"http://localhost:3000/query/device/",
+		method:"POST",
+		headers:{
+			"content-type":"application/json",
+			"Authorization" : "Bearer " + json_param.token
+		},
+		json:true,
+		body:devqr,
+		}, function(error, response, body) {
+			var DeviceID = body.DeviceID;
+		}
+	)
 
+	//Uses the device id obtained from above request
+	var unitqr = {"DeviceID" : DeviceID, "EmployeeRegistrationID" : "none", "UnitCondition" : "healthy"};
+	
+	request({
+		url:"http://localhost:3000/query/unit",
+		method:"POST",
+		headers:{
+			"content-type":"application/json",
+			"Authorization" : "Bearer " + json_param.token
+		},
+		json:true,
+		body:unitqr,
+		}, function(error, response, body) {
+			var unitList = body;
+		}
+	)
+
+	//Issue device
 	if (json_param.issue == true) {
-		//Issue device
+		
+		var unitissue = {"UnitID" : unitList[0], "EmployeeRegistrationID" : json_param.parameters.EmployeeID};
+		
+		request({
+			url:"http://localhost:3000/unit/issue",
+			method:"POST",
+			headers:{
+				"content-type":"application/json",
+				"Authorization" : "Bearer " + json_param.token
+			},
+			json:true,
+			body:unitissue,
+			}, function(error, response, body) {
+				
+			}
+		)
 
-		/*var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage, "OS" : json_param.parameters.os};
-		var dev_details = backindex.queryDB(devqr);
-		
-		//Uses the device id obtained from above function call
-		var unitqr = {"DeviceID" : dev_details.DeviceID, "EmployeeRegistrationID" : "none", "UnitCondition" : "healthy"};
-		var unit_details = backindex.query_unit(unitqr);
-		
-		var unitissue = {"UnitID" : unit_details[0], "EmployeeRegistrationID" : json_param.parameters.EmployeeID};
-		backindex.issueUnit(unitissue);	
-		result = "Issued Device Unit ID: " + unit_details[0];*/
-		callback("Device is issued with \n" + json_param);
+		result = unitList[0];
+		callback(result);
 	}
 	
+	//Query for available devices
 	else {
-		//Query for available devices
-		
-		/*var devqr = {"RAM" : json_param.parameters.ram, "Storage" : json_param.parameters.storage, "OS" : json_param.parameters.os};
-		var dev_details = backindex.queryDB(devqr);
 
-		//Uses the device id obtained from above function call
-		var unitqr = {"DeviceID" : dev_details.DeviceID, "EmployeeRegistrationID" : "none", "UnitCondition" : "healthy"};
-		var unit_details = backindex.query_unit(unitqr);
-		
-		if(unit_details.length > 0) {
+		if(unitList.length > 0) {
 			callback(true);
 		}
 
-		else if(unit_details.length == 0) {
+		else if(unitList.length == 0) {
 			callback(false);
-		}*/
-		callback("Device is checked for availability with \n" + json_param);
-
+		}
 	}	
 };
