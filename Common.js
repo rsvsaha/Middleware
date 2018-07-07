6ae6d2de0f0b6fea49f2845c6383d1d1f41672ff
k-manisha <30462640+k-manisha@users.noreply.github.com>
2018-07-07 16:06:19 +0530

    Update Common.js

--- Common.js
+++ Common.js
@@ -1,10 +1,10 @@
 module.exports.chat = function(req,callback){
 
  var data = req.body;
- var result=  data.query;
+ var query=  data.query;
  var token = data.token;	
  var userID = data.userId;
- callback(result, token,userID);
+ callback(query, token,userID);
 }
 
 module.exports.logger = function(req,callback){
