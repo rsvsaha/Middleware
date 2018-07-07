7446a924c612b19d5f13b323774b8cf68dfe286d
rsvsaha <36502870+rsvsaha@users.noreply.github.com>
2018-07-07 11:14:42 +0530

    Add files via upload

--- Chatbot_V1.js
+++ Chatbot_V1.js
@@ -1,7 +1,10 @@
 const apiai=require('apiai');
 const app=apiai("a005521e4a574ad59ab090190ab7a5d8"); //Put the required access token for the bot
 const bkconn=require('./backend_conn');	
-module.exports = function (sesId,query,callback) { 
+module.exports = function (token,user_Id,query,callback) { 
+    
+	var sesId=user_Id+token.substring(1,12);
+
     var options={
 		sessionId:sesId
 	};
@@ -121,17 +124,31 @@ module.exports = function (sesId,query,callback) {
 				 };
 				 
 
-				 bkconn.bkconn(requsest_to_backend,function(result){
+				 bkconn.bkconn(requsest_to_backend,function(result,id){
 							if(result==true){
 								reply={"answer":Result.fulfillment.speech};
-								//app.deleteContextsRequest(options);
-								callback(reply);
+								var delete_ContextsRequest=app.deleteContextsRequest(options);
+								delete_ContextsRequest.on('response',function(response){
+								callback(reply);	
+								});
+								delete_ContextsRequest.on('error',function(error){
+								console.log(error);
+								callbak(reply);	
+								});
 							}
 							else{
 
 								reply={"answer":"Sorry this device just went out of stock."};
 								//app.deleteContextsRequest(options);
+								var delete_ContextsRequest=app.deleteContextsRequest(options);
+								delete_ContextsRequest.on('response',function(response){
 								callback(reply);	
+								});
+								delete_ContextsRequest.on('error',function(error){
+								console.log(error);
+								callbak(reply);	
+								});
+	
 							}
 
 					
@@ -147,10 +164,10 @@ module.exports = function (sesId,query,callback) {
 			else if(Result.metadata.intentName=='DeviceStartContext-yes')
 			{
 				//console.log(Result.contexts[0][0]);
-
+				/*
 				var requsest_to_backend={"issue":true,
 				"parameters":null
-			};
+			};*/
 				//Accessories:Result.contexts[0].parameters.Accessories[],
 				
 				var requsest_to_backend={
@@ -167,19 +184,34 @@ module.exports = function (sesId,query,callback) {
 				 };
 				 
 
-				 bkconn.bkconn(requsest_to_backend,function(result){
+				 bkconn.bkconn(requsest_to_backend,function(result,id){
 							if(result==true){
 								reply={"answer":Result.fulfillment.speech};
-								//app.deleteContextsRequest(options);
-								callback(reply);		
+								var delete_ContextsRequest=app.deleteContextsRequest(options);
+								delete_ContextsRequest.on('response',function(response){
+								callback(reply);	
+								});
+								delete_ContextsRequest.on('error',function(error){
+								console.log(error);
+								callbak(reply);	
+								});
+				
 							}
 							else{
 
 								reply={"answer":"Sorry this device just went out of stock."};
 								//app.deleteContextsRequest(options);
+								var delete_ContextsRequest=app.deleteContextsRequest(options);
+								delete_ContextsRequest.on('response',function(response){
 								callback(reply);	
-							}
-						
+								});	
+								delete_ContextsRequest.on('error',function(error){
+								console.log(error);
+								callbak(reply);	
+								});
+	
+								}
+											
 
 							//INSERT WAY TO RESET CONTEXT
 
@@ -187,7 +219,39 @@ module.exports = function (sesId,query,callback) {
 		
 			}
 			
+			else if(Result.metadata.intentName=='DeviceQuery-no')
+			{
 
+				reply={"answer":Result.fulfillment.speech};
+				var delete_ContextsRequest=app.deleteContextsRequest(options);
+				delete_ContextsRequest.on('response',function(){
+				callback(reply);	
+				});
+				delete_ContextsRequest.on('error',function(error){
+				console.log(error);
+				callbak(reply);	
+				});
+
+
+
+			}
+			else if(Result.metadata.intentName=='DeviceStartContext-no')
+			{
+				reply={"answer":Result.fulfillment.speech};
+				var delete_ContextsRequest=app.deleteContextsRequest(options);
+				delete_ContextsRequest.on('response',function(response){
+				callback(reply);	
+				});
+
+				delete_ContextsRequest.on('error',function(error){
+				console.log(error);
+				callbak(reply);	
+				});
+
+
+
+
+			}
 
 
 
