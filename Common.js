442cd82399b4d648e2517bd5caca28cf439cac17
k-manisha <30462640+k-manisha@users.noreply.github.com>
2018-07-07 16:02:10 +0530

    Extract userID

--- Common.js
+++ Common.js
@@ -3,7 +3,8 @@ module.exports.chat = function(req,callback){
  var data = req.body;
  var result=  data.query;
  var token = data.token;	
- callback(result, token);
+ var userID = data.userId;
+ callback(result, token,userID);
 }
 
 module.exports.logger = function(req,callback){
